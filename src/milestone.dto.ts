8f010e4a631262526d2e01b7f084ee185e582e90
Sarab Ali <126464788+sarabdev@users.noreply.github.com>
2024-12-18 20:20:12 +0500

    add milestone types

---
+++ src/milestone.dto.ts
@@ -0,0 +1,137 @@
+export interface CreateMilestoneTemplateDto {
+  milestoneTemplateName: string;
+  milestoneTemplateFields?: number[];
+}
+
+export interface MilestoneTemplateRequest {
+  milestoneTemplateName: string;
+  milestoneTemplateFields?: number[];
+}
+
+export interface TransformMilestoneTemplateDto {
+  id: number;
+  templateName: string;
+  isActive: boolean;
+  createdAt: Date;
+  createdBy: number;
+  updatedAt: Date;
+  updatedBy: number;
+  isDeleted: boolean;
+  templateFields: {
+    id: number | null;
+    milestoneText: string | null;
+    milestoneType: string | null;
+  }[];
+}
+
+export interface UpdateMilestoneTemplateDto {
+  milestoneTemplateName?: string;
+  milestoneTemplateFields?: number[];
+  isActive?: boolean;
+  isDeleted?: boolean;
+}
+
+export interface MilestoneTemplateParamDto {
+  id: number;
+}
+
+export interface MilestoneTemplateDto {
+  id: number;
+  milestoneTemplateName: string;
+  createdAt: Date;
+  createdBy: number;
+  updatedAt: Date;
+  updatedBy: number;
+  isActive: boolean;
+  isDeleted: boolean;
+}
+
+export interface MilestoneTemplateFieldDto {
+  milestoneId: number;
+  milestoneTemplateId: number;
+  createdAt: Date;
+  createdBy: number;
+  updatedAt: Date;
+  updatedBy: number;
+  isActive: boolean;
+  isDeleted: boolean;
+}
+
+export interface MilestoneDto {
+  id: number;
+  milestoneText: string;
+  questionType: number;
+  createdAt: Date;
+  createdBy: number;
+  updatedAt: Date;
+  updatedBy: number;
+  isActive: boolean;
+  isDeleted: boolean;
+}
+
+export interface AuditFields {
+  createdAt: Date;
+  createdBy: number;
+  updatedAt: Date;
+  updatedBy: number;
+  isActive: boolean;
+  isDeleted: boolean;
+}
+
+export interface MilestoneTemplateAuditChanges {
+  templateName?: { new: string | null; old: string | null };
+  templateFields?: {
+    old: {
+      id: number | null;
+      milestoneText: string | null;
+      milestoneType: string | null;
+    } | null;
+    new: {
+      id: number | null;
+      milestoneText: string | null;
+      milestoneType: string | null;
+    } | null;
+  }[];
+}
+
+export interface MilestoneFields {
+  id: number | null;
+  milestoneText: string | null;
+  milestoneType: string | null;
+}
+
+export interface FieldsToInsert {
+  createdAt: Date;
+  createdBy: number;
+  updatedAt: Date;
+  updatedBy: number;
+  isActive: boolean;
+  isDeleted: boolean;
+  milestoneTemplateId: number;
+  milestoneId: number;
+}
+
+export interface MilestoneTemplateAuditTrailDto {
+  changes: Record<
+    string,
+    {
+      new: string;
+      old: string;
+    }
+  >;
+  updatedBy: string;
+  id: number;
+  updatedAt: Date;
+  milestoneTemplateId: number;
+  action: string;
+}
+
+export interface MilestoneAuditTrailWithUser {
+  milestoneTemplateId: number;
+  id: number;
+  updatedBy: number;
+  updatedAt: Date;
+  action: string;
+  changes: unknown;
+  username: string | null;
+}
