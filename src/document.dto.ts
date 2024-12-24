cbcae1fd9c4de7ace94b2e80a178413ea411f70a
Sarab Ali <126464788+sarabdev@users.noreply.github.com>
2024-12-24 14:00:56 +0500

    update document type

--- src/document.dto.ts
+++ src/document.dto.ts
@@ -1,9 +1,23 @@
-export interface DocumentList{
-    id: number;
-    entityType: string;
-    entityId: number;
-    fileName: string;
-    filePath: string;
-    updatedBy: string | null;
-    updatedAt: Date;
+export interface DocumentWithUser {
+  id: number;
+  entityType: string;
+  entityId: number;
+  fileName: string;
+  filePath: string;
+  updatedBy: string | null;
+  updatedAt: Date;
+}
+
+export interface DocumentDto {
+  id: number;
+  entityType: string;
+  entityId: number;
+  isActive: boolean;
+  isDeleted: boolean;
+  createdBy: number;
+  updatedBy: number;
+  createdAt: Date;
+  updatedAt: Date;
+  fileName: string;
+  filePath: string;
 }
