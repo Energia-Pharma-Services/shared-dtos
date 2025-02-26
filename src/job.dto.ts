4286f5360b40a49d8c19e83d268de86df64dcac3
khalyl <khalil@contrivers.dev>
2025-02-26 18:12:17 +0500

    Update job dtos

--- src/job.dto.ts
+++ src/job.dto.ts
@@ -2,29 +2,14 @@ export interface JobDto {
     id: number;
     interval: string | null;
     triggerDate: Date | null;
-    type: 'temporary_ma_date' | 'ma_expiry_date' | string;
+    fieldName: string;
     procedureId: number;
-    message: string;
     createdAt: Date;
     updatedAt: Date;
 }
 
-export interface CreateJobDto {
-    interval: string | null;
-    triggerDate?: Date | null;  // ✅ Allows optional null values
-    type: 'temporary_ma_date' | 'ma_expiry_date' | string;
-    data: {
-        procedureId: number;
-        message: string;
-    };
-}
+export type CreateJobDto =
+    Pick<JobDto, 'interval' | 'triggerDate' | 'fieldName' | 'procedureId'>
 
-export interface UpdateJobDto {
-    interval?: string | null;
-    triggerDate?: Date | null;  // ✅ Ensures compatibility with updates
-    type?: 'temporary_ma_date' | 'ma_expiry_date' | string;
-    data?: {
-        procedureId?: number;
-        message?: string;
-    };
-}
+export type UpdateJobDto = Partial<CreateJobDto>;
+
