93c2c6c62bb0ca9c5de91f0e2bac6fbc97445b10
Waleed Ahmed <125673034+waleedContriversDev@users.noreply.github.com>
2025-02-27 13:23:15 +0500

    Contact Dto Update

--- src/contact.dto.ts
+++ src/contact.dto.ts
@@ -169,7 +169,7 @@ export interface CountryLicenseDto extends AuditFields {
   id?: number;
   contactId: number;
   countryName: string;
-  gmpExpDate: Date;
+  gmpExpDate: Date | null;
 }
 
 /* eslint-disable @typescript-eslint/naming-convention */
